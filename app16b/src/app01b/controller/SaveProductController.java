96adcb6aded47e31538db3e359bfbf434f79c8a8
cyg_server <cyg_server@handsome>
2015-12-19 21:13:39 +0800

    app01b

---
+++ app16b/src/app01b/controller/SaveProductController.java
@@ -0,0 +1,38 @@
+package app01b.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import app01b.domain.Product;
+import app01b.form.ProductForm;
+
+public class SaveProductController implements Controller {
+	
+	@Override
+	public String handleRequest(HttpServletRequest request, 
+			HttpServletResponse response) {
+        ProductForm productForm = new ProductForm();
+        // populate form properties
+        productForm.setName(
+                request.getParameter("name"));
+        productForm.setDescription(
+                request.getParameter("description"));
+        productForm.setPrice(request.getParameter("price"));
+        
+        // create model
+        Product product = new Product();
+        product.setName(productForm.getName());
+        product.setDescription(productForm.getDescription());
+        try {
+        	product.setPrice(Float.parseFloat(
+        			productForm.getPrice()));
+        } catch (NumberFormatException e) {
+        }
+
+        // insert code to add product to the database
+
+        request.setAttribute("product", product);
+        return "/WEB-INF/jsp/ProductDetails.jsp";
+	}
+
+}
